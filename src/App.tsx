122d2a8a072a7c016276f57021c9fc28fb360d5e
Chris Mochinski <cmochinski@gmail.com>
2022-02-12 19:48:48 -0600

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const word: string = 'stuff';
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit this {word} and save to reload.
         </p>
         <a
           className="App-link"
